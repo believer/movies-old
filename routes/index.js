b85259b4e93f7ecb623981af8724f7b995aec7ef
Rickard Laurin <rickard.laurin@gmail.com>
2014-06-06 10:15:16 +0200

    added support for sending a user to lastfm lookup, added fallback to last played if nothing is currently playing

--- routes/index.js
+++ routes/index.js
@@ -462,14 +462,20 @@ exports.quiz = function (req,res) {
 exports.lastfm = function (req, res) {
   'use strict';
 
-  var url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user=iteam1337&api_key=59a34f30f3c5163f936e755463780ad2&format=json&limit=1';
+  // Get the POST body and split with an colon trying to find a user
+  // other than iteam1337, otherwise fall back to iteam1337
+  var body = req.body
+  ,   user = body.text.split(':')[1] ? body.text.split(':')[1].trim().split(' ')[0] : 'iteam1337'
+  ,   url = 'http://ws.audioscrobbler.com/2.0/?method=user.getrecenttracks&user={user}&api_key=59a34f30f3c5163f936e755463780ad2&format=json&limit=1';
 
-  request(url,function (err, response, body) {
+  request(url.replace('{user}',user),function (err, response, body) {
     var lastfm = JSON.parse(body);
-    lastfm = lastfm.recenttracks.track[0];
-    var when = lastfm['@attr'].nowplaying === 'true' ? 'Now playing' : 'Last song';
+    lastfm = lastfm.recenttracks.track[0] || lastfm.recenttracks.track;
+
+    var sendUser = (user !== 'iteam1337') ? ' (_' + user + '_)' : '';
+
     var obj = {
-      'text': lastfm.artist['#text'] + ' - ' + lastfm.name,
+      'text': lastfm.artist['#text'] + ' - ' + lastfm.name + sendUser,
     };
 
     res.send(obj);
